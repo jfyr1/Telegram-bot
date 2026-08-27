9d20e1e1715e184172303f8f2ff1d983cf73ee00
jfyr1 <jfrjlyla22@gmail.com>
2026-08-27 19:44:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.edit_message_text(text="المرحلة الثانية قيد التحديث.")
 
 if name == '__main__':
-    application = ApplicationBuilder().token('YOUR_TOKEN_HERE').build()
+    application = ApplicationBuilder().token('8925599691:AAHIGxwCVTb5hYQ-bCWKVS7-u__xduobniE
+').build()
 
     start_handler = CommandHandler('start', start)
     button_handler = CallbackQueryHandler(button)

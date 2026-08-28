b921621ef967e533bf0f97eee9ad5771fcb7943a
jfyr1 <jfrjlyla22@gmail.com>
2026-08-28 22:03:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from telegram.ext import (
 # =========================================================
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+ADMIN_ID = 5734654153
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN غير موجود في Environment Variables")
